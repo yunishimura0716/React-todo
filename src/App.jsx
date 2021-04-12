119544ecf8d1a8fc5a8949bf048e4a59cdf2d923
yunishimura0716 <38684796+yunishimura0716@users.noreply.github.com>
2021-04-11 23:35:20 -0700

    Created styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,31 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="Input ToDo" />
+        <button>Add</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">InComplete ToDo</p>
+        <ul>
+          <div className="list-row">
+            <li>Test Todo</li>
+            <button>Complete</button>
+            <button>Delete</button>
+          </div>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">Complete ToDo</p>
+        <ul>
+          <div className="list-row">
+            <li>Test Todo</li>
+            <button>Return</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
